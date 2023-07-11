ad0a5d2e1ea08bebfef78c4c4f0337619fd58059
Ruben Falla <rubenalejo96@gmail.com>
2023-07-11 13:39:55 -0500

    agregando todos los archivos

---
+++ src/Persona.java
@@ -0,0 +1,29 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
