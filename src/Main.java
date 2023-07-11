66e25c7aac3d10ea80ab67074c04dc38beb8046f
Ruben Falla <rubenalejo96@gmail.com>
2023-07-11 13:54:23 -0500

    Creando 2 Personas e Imprimiendo en consola sus datos

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setNombre("Ruben");
+        persona1.setTelefono(951425852);
+        persona1.setEdad(27);
+
+        Persona persona2 = new Persona();
+        persona2.setNombre("Carol");
+        persona2.setTelefono(945685258);
+        persona2.setEdad(41);
+
+        System.out.println("La Persona1 se llama "+persona1.getNombre()+ ", tiene "+
+                +persona1.getEdad()+ " y su numero telefonico es "+ persona1.getTelefono());
+
+        System.out.println("La Persona2 se llama "+persona2.getNombre()+ ", tiene "+
+                +persona2.getEdad()+ " y su numero telefonico es "+ persona2.getTelefono());
+    }
 }
